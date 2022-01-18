6dfd8bdcb52abb0f42336528e0ccfc24295b3c6b
HannesOberreiter <hoberreiter@gmail.com>
2022-01-18 17:37:56 +0100

    added missing url config for production

--- app/config/server.js
+++ app/config/server.js
@@ -3,6 +3,7 @@ const cronTasks = require("./cron-tasks");
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', ''),
   cron: {
     enabled: env.bool('CRON_ENABLED', true),
     tasks: cronTasks,
