c5b5e9d759759a4888ffacc460434ff9e1d577d6
HannesOberreiter <hoberreiter@gmail.com>
2022-01-21 13:36:05 +0100

    push

--- app/config/cron-tasks.js
+++ app/config/cron-tasks.js
@@ -2,17 +2,17 @@
 
 module.exports = {
     // https://dev.to/strapi/how-to-build-a-news-aggregator-app-using-strapi-and-nuxtjs-23hj
-    '0 0 0 * * *': {
+    '* * */1 * *': {
         task: async ({ strapi }) => {
             //await strapi.config.rss.updateFeed();
-            const res = await strapi.service('api::rss.rss').main(0);
-            strapi.log.debug(JSON.stringify(res));
+            console.log("I am running every 10 seconds >> " + new Date());
+            //const res = await strapi.service('api::rss.rss').main(2);
             return true;
         },
-        //options: {
+        options: {
             // List of valid timezones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List
-        //    tz: 'Europe/Vienna',
-        //},
+            //tz: 'Europe/Vienna',
+        },
     },
 };
  
